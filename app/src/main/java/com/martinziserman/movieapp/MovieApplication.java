b794a98d053a2c2c29df7556243dba1e6b6940a1
Martin Ziserman <martin@MacBook-Pro-de-Martin.local>
2016-05-12 18:05:49 +0200

    working api and picture"

--- app/src/main/java/com/martinziserman/movieapp/MovieApplication.java
+++ app/src/main/java/com/martinziserman/movieapp/MovieApplication.java
@@ -3,12 +3,15 @@ package com.martinziserman.movieapp;
 import android.app.Application;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+import com.martinziserman.movieapp.Network.LruBitmapCache;
 
 public class MovieApplication extends Application {
 
     private RequestQueue requestQueue;
     private static MovieApplication instance;
+    private ImageLoader imageLoader;
 
     public RequestQueue getRequestQueue() {
         return requestQueue;
@@ -24,6 +27,13 @@ public class MovieApplication extends Application {
         MovieApplication.instance = this;
 
         requestQueue = Volley.newRequestQueue(this);
+
+        LruBitmapCache lruBitmapCache = new LruBitmapCache(8 * 1024 * 1024);
+
+        imageLoader = new ImageLoader(requestQueue, lruBitmapCache);
     }
 
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
 }
