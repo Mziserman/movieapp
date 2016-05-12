56bd7cd93c966184a7f2e7bb853fa8cef0dafbcb
Martin Ziserman <martin@MacBook-Pro-de-Martin.local>
2016-05-12 11:34:18 +0200

    init

---
+++ app/src/main/java/com/martinziserman/movieapp/model/Movie.java
@@ -0,0 +1,16 @@
+package com.martinziserman.movieapp.model;
+
+/**
+ * Created by martin on 12/05/2016.
+ */
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
